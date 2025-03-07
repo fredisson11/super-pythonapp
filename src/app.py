e3fae6aab6d6efcf472bcd5fab883ebce8026e0e
fredisson11 <185803622+fredisson11@users.noreply.github.com>
2025-03-07 14:06:06 +0100

    add linting

--- src/app.py
+++ src/app.py
@@ -79,11 +79,12 @@ def external_call():
     try:
         response = requests.get(external_url)
         return Response(
-            f"External call response: {response.text}", status=response.status_code
+            f"External call response: {response.text}",
+            status=response.status_code
         )
     except Exception as e:
-        return Response(f"Error calling external endpoint: {str(e)}", status=500)
-
+        return Response(f"Error calling external endpoint: {str(e)}",
+                        status=500)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
