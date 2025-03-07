25bbc185db65407f2c8a0f8b875092a26eca9adb
fredisson11 <185803622+fredisson11@users.noreply.github.com>
2025-03-07 15:18:11 +0100

    add unit tests

--- src/app.py
+++ src/app.py
@@ -79,12 +79,10 @@ def external_call():
     try:
         response = requests.get(external_url)
         return Response(
-            f"External call response: {response.text}",
-            status=response.status_code
+            f"External call response: {response.text}", status=response.status_code
         )
     except Exception as e:
-        return Response(f"Error calling external endpoint: {str(e)}",
-                        status=500)
+        return Response(f"Error calling external endpoint: {str(e)}", status=500)
 
 
 if __name__ == "__main__":
